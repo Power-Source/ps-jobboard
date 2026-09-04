b573d82408e9060e289f0c42ef7d28c6785013ee
DerN3rd <dern3rd@outlook.at>
2026-09-04 03:11:31 +0200

    feat: Update version to 1.0.6 and ClassicPress compatibility to 2.7.2

--- jobs-experts.php
+++ jobs-experts.php
@@ -3,10 +3,10 @@
  * Plugin Name: PS-Jobboard
  * Plugin URI: https://psource.eimen.net/wiki/ps-jobboard/
  * Description: Bringe Menschen mit Projekten und Branchenfachleute zusammen - es ist mehr als eine durchschnittliche Jobbörse.
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: PSOURCE
  * Author URI: https://psource.eimen.net
- * ClassicPress: 2.7.1
+ * ClassicPress: 2.7.2
  * Text Domain: psjb
  * Domain Path: /languages
  * Network: false
@@ -46,7 +46,7 @@ class Jobs_Experts {
 	public $domain;
 	public $prefix;
 
-	public $version = "1.0.5";
+	public $version = "1.0.6";
 	public $db_version = "1.0";
 
 	public $global = array();
